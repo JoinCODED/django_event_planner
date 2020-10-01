b7be2f6ef24eb6c105deda63f8dcdec2487094f7
KartikTakyar0046 <42770126+KartikTakyar0046@users.noreply.github.com>
2020-10-01 12:37:23 +0530

    Update models.py

--- events/models.py
+++ events/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+
+class Event(models.Model):
+  event_name=models.CharField(max_length=100)
+  event_starttime=models.DateTimeField(auto_now_add=True)
+  event_endtime=models.DateField(default=now,editable=True)
+  def __str__(self):
+    return f'{self.event_name}'
+    
